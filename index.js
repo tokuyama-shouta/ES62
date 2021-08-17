0b29fdfb7f251d2d84789ee66251494a857a11b3
tokuyama shouta <toku1031ts@gmail.com>
2021-08-17 20:35:20 +0900

    関数の引数で分割代入

--- index.js
+++ index.js
@@ -332,3 +332,16 @@ const { type,amount } = expense;
 type;
 amount;
 
+let savedFile = {
+  extension: 'jpg',
+  name: 'profile',
+  size: 14040
+}
+
+function fileSummary({ name, extension, size },{ username }){
+
+  return `${username}:${name}.${extension}の容量は${size}です。`
+}
+
+fileSummary(saveFile,{username: 'ken'});
+
