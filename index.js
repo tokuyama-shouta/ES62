19087d5282f62cbf1361dbfa4f39ce5a6b08e6ea
tokuyama shouta <toku1031ts@gmail.com>
2021-08-26 20:37:48 +0900

    generator

--- index.js
+++ index.js
@@ -471,6 +471,22 @@ function* numbers() {
 const gen = numbers();
 gen.next();
 
+function* shopping() {
+  //歩道
+
+  //歩道を歩いてお店に行く
+
+  //お店に到着したのでお金を持ってお店に入る
+  const stuffFromStore = yield 'お店';
+
+  //家に歩いてかえる
+  return stuffFromStore;
+}
+
+//お店関連の世界
+const gen = shopping();
+gen.next(); //家から歩道に出る
+gen.next('日用品'); // お店で買い物をして日用品を持って歩道に出る。
 
 
 
